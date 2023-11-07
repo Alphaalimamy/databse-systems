465833cb095ae93789ca1a93b439513f08b8b4ca
omarleee <leighomar12654@gmail.com>
2023-11-07 12:00:17 +0100

    I change the name of the developers

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'LIMKOKWING UNIVERSITY OF CREATIVE TECHNOLOGY',
-  tagline: 'TECH CONNECT',
+  tagline: 'Django Girls Organization',
   // favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
